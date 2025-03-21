b767e1f48f67f5fafc06cea0cca41f05bd4c3a41
Gonorreator0 <146373349+Gonorreator0@users.noreply.github.com>
2025-03-21 17:42:49 -0500

    Vista Login/Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../views/Login.vue'
 
+// Definimos las rutas de la aplicación en un array de objetos
 const routes = [
   {
     path: '/',
@@ -10,16 +12,24 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    // División de código a nivel de ruta
+    // Esto genera un archivo separado (about.[hash].js) para esta ruta
+    // que se carga de forma diferida (lazy-loaded) cuando se visita la ruta.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
+// Creamos el enrutador utilizando createRouter
+// y definimos que la historia será de tipo Hash (URL con #)
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 })
 
+// Exportamos el enrutador para poder usarlo en el resto de la aplicación
 export default router
